c8537a35f696a600ef194b19be07e8cd2fc8b5a3
Michał <mwargan@gmail.com>
2020-10-06 13:20:48 +0200

    Added ability to pass date_preset
    Date preset reading: https://developers.facebook.com/docs/marketing-api/insights/parameters/v8.0

--- src/Insights.php
+++ src/Insights.php
@@ -3,9 +3,9 @@
 namespace Edbizarro\LaravelFacebookAds;
 
 use FacebookAds\Object\Ad;
+use FacebookAds\Object\AdAccount;
 use FacebookAds\Object\AdSet;
 use FacebookAds\Object\Campaign;
-use FacebookAds\Object\AdAccount;
 use FacebookAds\Object\Values\AdsInsightsLevelValues;
 
 class Insights
@@ -25,10 +25,14 @@ class Insights
         $params['time_increment'] = $params['time_increment'] ?? '1';
         $params['limit'] = $params['limit'] ?? 100;
         $params['level'] = $level;
-        $params['time_range'] = [
-            'since' => $period->startDate->format('Y-m-d'),
-            'until' => $period->endDate->format('Y-m-d'),
-        ];
+        if (isset($params['date_preset'])) {
+            $params['date_preset'] = $params['date_preset'] ?? 'lifetime';
+        } else {
+            $params['time_range'] = [
+                'since' => $period->startDate->format('Y-m-d'),
+                'until' => $period->endDate->format('Y-m-d'),
+            ];
+        }
 
         $apiResponse = $levelClass->getInsights($fields, $params);
 
